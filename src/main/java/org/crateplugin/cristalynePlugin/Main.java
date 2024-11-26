ccce0fdbfe6b2b1eabf8ce5fd9286738d0fbdb36
Kilian <k.coubard2710@gmail.com>
2024-11-26 20:07:41 +0100

    ajout de la verification du lancement/arret du plugin

--- src/main/java/org/crateplugin/cristalynePlugin/Main.java
+++ src/main/java/org/crateplugin/cristalynePlugin/Main.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    public static String pluginPrefix = "§8[§9My_ideas§8] §7";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        getServer().getConsoleSender().sendMessage(pluginPrefix + "le plugin est §alancé!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        getServer().getConsoleSender().sendMessage(pluginPrefix + "le plugin est §carreté!");
+
     }
 }
