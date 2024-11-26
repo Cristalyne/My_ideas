a6983de1e159fc155405fdb95b901a06e0d8432a
Kilian <k.coubard2710@gmail.com>
2024-11-26 20:28:50 +0100

    ajout d'une static ItemStack & ajout d'item dans le menu

--- src/main/java/org/crateplugin/cristalynePlugin/commands/Settings.java
+++ src/main/java/org/crateplugin/cristalynePlugin/commands/Settings.java
@@ -1,11 +1,14 @@
 package org.crateplugin.cristalynePlugin.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Settings implements CommandExecutor {
     @Override
@@ -15,10 +18,27 @@ public class Settings implements CommandExecutor {
 
             Inventory SettingsMenu = Bukkit.createInventory(null, 27, "§6§lSettings Menu");
 
+            for(int i = 0; i < SettingsMenu.getSize(); i++){
+
+                    SettingsMenu.setItem(i, item(Material.GRAY_STAINED_GLASS_PANE, " ", 1));
+
+            }
+
             player.openInventory(SettingsMenu);
 
         }
 
         return false;
     }
+
+    public static ItemStack item(Material material, String string, int quantity){
+
+        ItemStack i = new ItemStack(material, quantity);
+        ItemMeta imeta = i.getItemMeta();
+        imeta.setDisplayName(string);
+        i.setItemMeta(imeta);
+
+        return i;
+    }
+
 }
